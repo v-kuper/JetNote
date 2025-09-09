6f98921ae2506cf320585c3cc2ea392e99036f3b
v-kuper <vkuper7@gmail.com>
2025-09-09 23:00:48 +0300

    feat: add suspend

--- app/src/main/java/com/example/jetnote/data/NoteDatabaseDao.kt
+++ app/src/main/java/com/example/jetnote/data/NoteDatabaseDao.kt
@@ -29,32 +29,32 @@ interface NoteDatabaseDao {
      * @return Заметка с указанным id.
      */
     @Query("SELECT *from notes_tbl where id =:id")
-    fun getNoteById(id: String): Note
+    suspend fun getNoteById(id: String): Note
 
     /**
      * Вставка новой заметки в базу данных или замена существующей при конфликте.
      * @param note Заметка для вставки или замены.
      */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insert(note: Note)
+    suspend fun insert(note: Note)
 
     /**
      * Обновление существующей заметки в базе данных, заменяя при конфликте.
      * @param note Заметка для обновления.
      */
     @Update(onConflict = OnConflictStrategy.REPLACE)
-    fun update(note: Note)
+    suspend fun update(note: Note)
 
     /**
      * Удаление всех заметок из таблицы notes_tbl.
      */
     @Query("DELETE from notes_tbl")
-    fun deleteAll()
+    suspend fun deleteAll()
 
     /**
      * Удаление одной заметки из базы данных.
      * @param note Заметка для удаления.
      */
     @Delete
-    fun deleteNote(note: Note)
+    suspend fun deleteNote(note: Note)
 }
